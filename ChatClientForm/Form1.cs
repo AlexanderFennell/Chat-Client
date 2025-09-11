88c693b0a6a973ff04f23362782d80fc79ac4415
Alexander-Fennell <alexander@fennell.se>
2025-09-11 20:44:54 +0200

    Forgor to push like 2 years ago, now i have added alot oops

--- ChatClientForm/Form1.cs
+++ ChatClientForm/Form1.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Net.Sockets;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Web;
 using System.Windows.Forms;
@@ -15,15 +16,19 @@ namespace ChatClientForm
     public partial class Form1 : Form
     {
 
-        public List<string> connectedUserNames = new List<string>();
+        //public List<string> connectedUserNames = new List<string>();
+        string[] connectedUserNames = null;
         public string userName;
         public string message;
         public string messageToServer;
+
+        public TcpClient client;
+        NetworkStream stream;
         public Form1()
         {
             InitializeComponent();
             chatMessageText.Text = "";
-            
+            connectedUserList.Text = "";
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -58,10 +63,11 @@ namespace ChatClientForm
             }
             else
             {
-                TcpClient client = new TcpClient("127.0.0.1", 8000);
-                NetworkStream stream = client.GetStream();
+                client = new TcpClient("127.0.0.1", 8000);
+                stream = client.GetStream();
                 byte[] data = Encoding.UTF8.GetBytes(userName);
                 stream.Write(data, 0, data.Length);
+                Task.Run(() => UpdateChatPanel());
             }
         }
 
@@ -82,26 +88,66 @@ namespace ChatClientForm
             }
             else
             {
-                
                 message = messageInputField.Text;
                 messageToServer = $"{userName}: {message}"; //Add timestamps to messages format "[12:00] Alex: Hello world"
                                                             //Send message to the server.
-                chatMessageText.Text += messageToServer + Environment.NewLine;
-                messageInputField.Clear();
+                byte[] data = Encoding.UTF8.GetBytes(messageToServer);
+                stream.Write(data, 0, data.Length);
+                
             }
                 
         }
 
         private void UpdateChatPanel()
         {
+            byte[] buffer = new byte[4028];
+            while (true) 
+            {
+                int bytesRead = stream.Read(buffer, 0, buffer.Length);
+                if ( bytesRead == 0)
+                {
+                    break;
+                }
+                
+                message = Encoding.UTF8.GetString(buffer, 0 ,buffer.Length);
+                //Todo add me: when client writes instead of username: 
+                this.Invoke((MethodInvoker)(() =>
+                {
+                    if (message.StartsWith(userName))
+                    {
+                        chatMessageText.Text += message;
+                        chatMessageText.Text += Environment.NewLine;
+                        messageInputField.Clear();
+                        
+                    }
+                    else if (message.StartsWith("/c/n"))
+                    {
+                        UpdateConnectedUsersText(message);
+                    }
+                    else
+                    {
+                        chatMessageText.Text += message;
+                        chatMessageText.Text += Environment.NewLine;
+                        messageInputField.Clear();
+
+
+                    }
+                    buffer = new byte[4028];
+                }));
+            }
             
         }
-        private void UpdateConnectedUsersText()
+
+        private void UpdateConnectedUsersText(string message)
         {
+            message = message.Replace("/c", "");
+            connectedUserNames = Regex.Split(message, @"/n/");
             foreach (var user in connectedUserNames)
             {
-                connectedUserList.Text += user + Environment.NewLine;
-               
+                connectedUserList.Text += user;
+                connectedUserList.Text += Environment.NewLine;
+                Console.WriteLine($"loop: {message} | {user}");
+
             }
         }
         private void panel3_Paint(object sender, PaintEventArgs e)
